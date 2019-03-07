3ab57ecb5d49e8fd2547fb8347302487fcc6a13a
Andreani Soupashi <wf003668@live.reading.ac.uk>
2019-03-07 15:29:07 +0000

    Code refactored to generate a set number of posts and refresh them.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ firebase.auth().onAuthStateChanged(function(user){
 if (user){
   var isAnonymous = user.isAnonymous;
   var uid = user.uid;
-  showpost();
+  showposts(8);
 } else {
 
 }
@@ -27,43 +27,77 @@ function postpost(){
     }
     
      ref.push(data)
-     showpost();
+     RefreshPosts(8);
 }
-
-function showpost(){
-  var data = {
-    PostTitle: title,
-    Question: question
-    }
-    var counter = 0;
-    var t = 't';
-       var q = 'q';
-    console.log(title)
-    var title = document.getElementsByClassName("post");
-    counter = title.length-1;
-    var query = firebase.database().ref("Posts").limitToLast(6);
+//This function generates a div class with class post
+function GeneratePost(t, q, c)
+{
+  var qid = "q";
+  var tid = "t";
+  var QID = "";
+  var TID ="";
+  QID = QID.concat(qid, String(c));
+  TID = TID.concat(tid, String(c));
+       var newPost = document.createElement('div');
+       newPost.className = 'post';
+       var heading3 = document.createElement('h3');
+       var posttitle = document.createTextNode(t);
+       heading3.appendChild(posttitle);
+       heading3.id = TID;
+       newPost.appendChild(heading3);
+       var heading4 = document.createElement('h4');
+       var question = document.createTextNode(q);
+       heading4.appendChild(question);
+       heading4.id = QID;
+       newPost.appendChild(heading4);
+       document.getElementById('postArea').appendChild(newPost);
+       console.log(TID);
+       console.log(QID);
+}
+//This pulls the last x posts from the database and generates a list of posts 
+//Firebase cannot sort in descending order, unfortunately.
+function showposts(x){
+  
+    counter = x-1;
+    var query = firebase.database().ref("Posts").limitToLast(x);
     query.once("value").then(function(snapshot){
     snapshot.forEach(function(childSnapshot){
-      
-       var key = childSnapshot.key;
-      
-       //console.log(key);
-       var childData = childSnapshot.val();
-      var currentT = '';
-      var currentQ = '';
-      currentT = currentT.concat(t, counter);
-      currentQ = currentQ.concat(q, counter);
-      console.log(currentQ)
-      document.getElementById(currentT).innerHTML= String(childData.PostTitle);
-      document.getElementById(currentQ).innerHTML = String(childData.Question);
-      counter--
-       if (counter < 0){return true;}
-       //console.log(childData);
+    var childData = childSnapshot.val();
+    GeneratePost(childData.PostTitle, childData.Question, counter);
+     counter--;
     });
     
     });
 }
-
+//This function gets a post created by the Generate posts function
+//it recreates its id and edits the attributes there
+//This ensures that the total number of posts stays constant
+function RefreshPost(t,q, c){
+  var qid = "q";
+  var tid = "t";
+  var QID = "";
+  var TID ="";
+  QID = QID.concat(qid, String(c));
+  TID = TID.concat(tid, String(c));
+  document.getElementById(TID).innerHTML = t;
+  document.getElementById(QID).innerHTML = q;
+  console.log(QID);
+  console.log(TID);
+}
+//This refreshes x number of posts
+function RefreshPosts(x){
+  
+  var counter = x-1;
+  var query = firebase.database().ref("Posts").limitToLast(x);
+    query.once("value").then(function(snapshot){
+    snapshot.forEach(function(childSnapshot){
+    var childData = childSnapshot.val();
+      RefreshPost(childData.PostTitle, childData.Question, counter);
+      counter--
+    });
+    
+    });
+}
 
 
 
